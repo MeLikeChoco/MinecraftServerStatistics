3da81de1dc9d44a1b12ef7910c692e10e7a83d5c
Edgar Chang <melikechoco@gmail.com>
2018-10-17 09:33:52 -0400

    starting new module

---
+++ MinecraftServerStatistics/Models/ServerLists/Minecraft-ServerNet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+
+namespace MinecraftServerStatistics.Models.ServerLists
+{
+    public class Minecraft_ServerNet : Scraper
+    {
+
+        public Minecraft_ServerNet() : base("https://minecraft-server.net/") { }
+
+        protected override Task<int> GetFeaturedServerCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetFeatures(IDocument dom, List<string> features)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetName(IDocument dom)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetServerIP(IDocument dom)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<List<string>> GetServerLinks(List<string> links, int page, int remaining)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetVersion(IDocument dom)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetWebsite(IDocument dom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
